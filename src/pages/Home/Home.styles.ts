1b1f25294148d44d8b80dc8c192dcb1e31a267c4
Jan Jędrzejewski <janjedrzejewski@gmail.com>
2022-04-03 17:40:24 +0200

    Home page added

---
+++ src/pages/Home/Home.styles.ts
@@ -0,0 +1,19 @@
+import { styled } from '@mui/material/styles';
+
+export const Wrapper = styled('div')`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 24px;
+`;
+
+export const TitleWrapper = styled('div')`
+  margin: 0 0 24px 0;
+`;
+
+export const SearchBarWrapper = styled('div')`
+  width: 50%;
+`;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,20 @@
+import SearchBar from '@components/SearchBar/SearchBar';
+import { Typography } from '@mui/material';
+import { FC } from 'react';
+
+import * as Styled from './Home.styles';
+
+const Home: FC = () => {
+  return (
+    <Styled.Wrapper>
+      <Styled.TitleWrapper>
+        <Typography variant="h5">Search for stuff</Typography>
+      </Styled.TitleWrapper>
+      <Styled.SearchBarWrapper>
+        <SearchBar />
+      </Styled.SearchBarWrapper>
+    </Styled.Wrapper>
+  );
+};
+
+export default Home;
