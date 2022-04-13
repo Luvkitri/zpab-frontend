2aaf330ba6dacde8143ade4eca2800460ff04344
Adam Kowalczyk <kombajno678@gmail.com>
2022-04-13 09:29:55 +0200

    login endpoint in user service

--- src/services/User/User.service.ts
+++ src/services/User/User.service.ts
@@ -3,13 +3,31 @@ import { ResponseUserDataProps } from './User.types';
 
 class User {
   endpoint: string;
+  login_endpoint: string;
 
   constructor() {
     this.endpoint = 'users';
+    this.login_endpoint = 'login';
   }
 
   getAll(): Promise<ResponseUserDataProps> {
     return axios.get(`${this.endpoint}`);
+  }
+
+  login(email: string, password: string): Promise<any> {
+    return new Promise<string>((resolve, reject) => {
+      axios.post(`${this.login_endpoint}`, { "email": email, "password": password })
+        .then(response => {
+          console.log(response);
+          if (response.status == 200) {
+            resolve(response.data);
+          } else {
+            reject(`Login request returned status: ${response.status}`);
+          }
+        }).catch(reason => {
+          reject(`Login request returned status: ${reason}`);
+        });
+    })
   }
 }
 
