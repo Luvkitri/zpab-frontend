012e7669f6df828e72c52b7abc1767a1ad6c7050
Jan Jędrzejewski <janjedrzejewski@gmail.com>
2022-04-15 19:09:45 +0200

    On page load, load data

--- src/components/Accommodations/Accommodations.tsx
+++ src/components/Accommodations/Accommodations.tsx
@@ -1,5 +1,5 @@
 import { AccommodationDataProps } from '@services/Accomodation/Accommodation.types';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { AccommodationsProps } from './Accommodations.types';
 import * as Styled from './Accommodations.styles';
@@ -10,21 +10,22 @@ const Accommodations: FC<AccommodationsProps> = () => {
     Array<AccommodationDataProps>
   >([]);
 
-  const loadData = async () => {
-    try {
-      const accommodationsResults = await AccommodationService.getAll();
-      setAccommodations(accommodationsResults.data);
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
+  useEffect(() => {
+    const initData = async () => {
+      try {
+        const accommodationsResults = await AccommodationService.getAll();
+        setAccommodations(accommodationsResults.data);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    };
+
+    initData();
+  }, []);
 
   return (
     <Styled.ResultWrapper>
-      <Styled.Button variant="outlined" onClick={loadData}>
-        Get accommodations
-      </Styled.Button>
       {accommodations?.map((data, index) => (
         <p key={`accommodationsdata${index}`}>{JSON.stringify(data)}</p>
       ))}
