46a42e185611e333fe2d2c81619c0845a94cec97
Adam Kowalczyk <kombajno678@gmail.com>
2022-04-16 15:14:59 +0200

    header buttons based on user state

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,47 @@
 import { Button, Typography } from '@mui/material';
 import * as Styled from './Header.styles';
 import { useNavigate } from 'react-router-dom';
+import { isSignedIn, signOut, getUserEmail, onSignChange } from '@utils/login';
+import { render } from 'react-dom';
+import { useState } from 'react';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [signedIn, setSignedIn] = useState(isSignedIn());
+  const updateState = () => {
+    setSignedIn(isSignedIn());
+  };
+  onSignChange(updateState);
+
+  const handleSignOutButtonClick = () => {
+    signOut();
+    navigate('/');
+  };
+  let buttons: JSX.Element = <b></b>;
+  const buttonsSignedIn = (
+    <Styled.ButtonsWrapper>
+      <Button color="inherit" onClick={handleSignOutButtonClick}>
+        Logout {getUserEmail()}
+      </Button>
+    </Styled.ButtonsWrapper>
+  );
+
+  const buttonsAnonymous = (
+    <Styled.ButtonsWrapper>
+      <Button color="inherit" onClick={() => navigate('/login')}>
+        Login
+      </Button>
+      <Button color="inherit" onClick={() => navigate('/register')}>
+        Register
+      </Button>
+    </Styled.ButtonsWrapper>
+  );
+
+  if (signedIn) {
+    buttons = buttonsSignedIn;
+  } else {
+    buttons = buttonsAnonymous;
+  }
 
   return (
     <Styled.Wrapper>
@@ -15,12 +53,7 @@ const Header = () => {
           <Button color="inherit" onClick={() => navigate('/')}>
             Home
           </Button>
-          <Button color="inherit" onClick={() => navigate('/login')}>
-            Login
-          </Button>
-          <Button color="inherit" onClick={() => navigate('/register')}>
-            Register
-          </Button>
+          {buttons}
         </Styled.Toolbar>
       </Styled.AppBar>
     </Styled.Wrapper>
