c8277132da96cc553635be33fc82f73778569ffc
Adam Kowalczyk <kombajno678@gmail.com>
2022-04-21 15:27:10 +0200

    assert user logged in on new accommodation page

--- src/pages/AddAccommodation/AddAccommodation.tsx
+++ src/pages/AddAccommodation/AddAccommodation.tsx
@@ -1,11 +1,28 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import * as Styled from '@layout/Layout.styles';
 import { useNavigate } from 'react-router-dom';
 import AccommodationForm from '@components/AccommodationForm';
 import { AccommodationDataProps } from '@services/Accommodation/Accommodation.types';
+import { UserDataProps } from '@services/User/User.types';
+import { getUserId, isSignedIn } from '@utils/login';
+import User from '@services/User/User.service';
 
 const AddAccommodation: FC = () => {
   const navigate = useNavigate();
+
+  const [user, setUser] = useState<UserDataProps>();
+
+  useEffect(() => {
+    const loadData = async () => {
+      let userId = getUserId();
+      setUser((await User.get(userId)).data);
+    };
+    if (!isSignedIn()) {
+      navigate('/login');
+    } else {
+      loadData();
+    }
+  }, []);
 
   const onAdd = (acc: AccommodationDataProps) => {
     console.log({ acc });
